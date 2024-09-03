a29599d69832a297ee306ce776ac042707c7b8f9
BrunaGomesTW <61760654+BrunaGomes01@users.noreply.github.com>
2024-09-03 17:53:35 -0300

    fix: então ta ne

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -4,13 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
 
 import { TaskService } from './service/task.service';
-import { UserService } from '../user/service/user.service';
 import { TaskController } from './controller/task.controller';
 import { User } from '../user/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task, User])],
-  providers: [TaskService, Logger, UserService],
+  providers: [TaskService, Logger],
   exports: [TaskService],
   controllers: [TaskController],
 })

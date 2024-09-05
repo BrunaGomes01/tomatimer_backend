e02c5d00c713a66495b88e1da6cc169e31916807
BrunaGomesTW <61760654+BrunaGomes01@users.noreply.github.com>
2024-09-04 21:27:30 -0300

    desabilita cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.enableCors();
+  app.enableVersioning();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
